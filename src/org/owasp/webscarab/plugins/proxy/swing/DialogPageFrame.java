f86db9371818ff69c1d9596082e252e64cb405e4
Rogan Dawes <rogan@dawes.za.net>
2006-06-29 12:51:03 +0200

    make use of the standard dialog look and feel. No need for custom code

--- src/org/owasp/webscarab/plugins/proxy/swing/DialogPageFrame.java
+++ src/org/owasp/webscarab/plugins/proxy/swing/DialogPageFrame.java
@@ -3,7 +3,6 @@
  */
 package org.owasp.webscarab.plugins.proxy.swing;
 
-import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
@@ -13,8 +12,6 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
-import javax.swing.JPanel;
-import javax.swing.JSeparator;
 import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
@@ -22,7 +19,6 @@ import org.springframework.richclient.command.ActionCommand;
 import org.springframework.richclient.command.ActionCommandInterceptor;
 import org.springframework.richclient.dialog.DialogPage;
 import org.springframework.richclient.dialog.support.DialogPageUtils;
-import org.springframework.richclient.util.GuiStandardUtils;
 
 /**
  * @author rdawes
@@ -34,27 +30,14 @@ public class DialogPageFrame {
 
 	private JFrame frame;
 
-	private ActionCommand cancelCommand;
-
 	private boolean actionPerformed = false;
 
 	public DialogPageFrame(DialogPage page, ActionCommand okCommand,
-			ActionCommand cancelCommand) {
-		this.cancelCommand = cancelCommand;
+			final ActionCommand cancelCommand) {
 		frame = new JFrame();
 		Container pane = frame.getContentPane();
 
-		DialogPageUtils.adaptPageCompletetoGuarded(page, okCommand);
-		JPanel titlePaneContainer = new JPanel(new BorderLayout());
-		titlePaneContainer.add(DialogPageUtils.createTitlePane(page)
-				.getControl());
-		titlePaneContainer.add(new JSeparator(), BorderLayout.SOUTH);
-		pane.add(titlePaneContainer, BorderLayout.NORTH);
-		pane.add(DialogPageUtils.createButtonBar(new ActionCommand[] {
-				okCommand, cancelCommand }), BorderLayout.SOUTH);
-		JComponent pageControl = page.getControl();
-		GuiStandardUtils.attachDialogBorder(pageControl);
-		pane.add(pageControl);
+		pane.add(DialogPageUtils.createStandardView(page, okCommand, cancelCommand));
 
 		ActionCommandInterceptor interceptor = new ActionCommandInterceptor() {
 			public void postExecution(ActionCommand command) {
@@ -63,7 +46,6 @@ public class DialogPageFrame {
 					lock.notify();
 				}
 			}
-
 			public boolean preExecution(ActionCommand command) {
 				return true;
 			}
@@ -81,7 +63,12 @@ public class DialogPageFrame {
 		frame.setTitle(page.getTitle());
 		frame.setIconImage(page.getImage());
 		frame.setSize(800, 600); // FIXME - we need some way of tracking the preferred size/location
-		frame.addWindowListener(new CloseListener());
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosed(WindowEvent e) {
+				if (!actionPerformed)
+					cancelCommand.execute();
+			}
+		});
 	}
 
 	public void showAsDialog() {
@@ -100,11 +87,4 @@ public class DialogPageFrame {
 		frame.dispose();
 	}
 
-	private class CloseListener extends WindowAdapter {
-		@Override
-		public void windowClosed(WindowEvent e) {
-			if (!actionPerformed)
-				cancelCommand.execute();
-		}
-	}
 }
