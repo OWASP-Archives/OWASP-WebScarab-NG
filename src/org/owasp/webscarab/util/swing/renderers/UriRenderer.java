f9b7593846c471103c2291c9b14f12d9ba0afc29
Rogan Dawes <rogan@dawes.za.net>
2006-07-27 13:17:55 +0200

    Add the UriRenderer (belatedly)

---
+++ src/org/owasp/webscarab/util/swing/renderers/UriRenderer.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.owasp.webscarab.util.swing.renderers;
+
+import java.awt.Component;
+import java.net.URI;
+
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+import org.owasp.webscarab.util.UrlUtils;
+
+/**
+ * @author rdawes
+ *
+ */
+public class UriRenderer extends DefaultTreeCellRenderer {
+
+	/* (non-Javadoc)
+	 * @see javax.swing.tree.DefaultTreeCellRenderer#getTreeCellRendererComponent(javax.swing.JTree, java.lang.Object, boolean, boolean, boolean, int, boolean)
+	 */
+	@Override
+	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+		// TODO Auto-generated method stub
+		Component c = super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf,
+				row, hasFocus);
+		if (value instanceof URI) {
+			URI uri = (URI) value;
+			URI parent = UrlUtils.getParent(uri);
+			if (parent != null) {
+				setText(uri.toString().substring(parent.toString().length()));
+			}
+		}
+		return c;
+	}
+
+}
