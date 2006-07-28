81db443ff5ac8bfe5950a514cf3775bd9c798890
Rogan Dawes <rogan@dawes.za.net>
2006-07-28 15:44:16 +0200

    fix a warning

--- src/org/owasp/webscarab/util/swing/renderers/UriRenderer.java
+++ src/org/owasp/webscarab/util/swing/renderers/UriRenderer.java
@@ -17,12 +17,13 @@ import org.owasp.webscarab.util.UrlUtils;
  */
 public class UriRenderer extends DefaultTreeCellRenderer {
 
+	private static final long serialVersionUID = 6598912493169873948L;
+
 	/* (non-Javadoc)
 	 * @see javax.swing.tree.DefaultTreeCellRenderer#getTreeCellRendererComponent(javax.swing.JTree, java.lang.Object, boolean, boolean, boolean, int, boolean)
 	 */
 	@Override
 	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean sel, boolean expanded, boolean leaf, int row, boolean hasFocus) {
-		// TODO Auto-generated method stub
 		Component c = super.getTreeCellRendererComponent(tree, value, sel, expanded, leaf,
 				row, hasFocus);
 		if (value instanceof URI) {
