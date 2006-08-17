8833b82204e2e88ca3d5e526dc75c70538ab6afc
Rogan Dawes <rogan@dawes.za.net>
2006-08-17 16:05:21 +0200

    [Proxy] make the proxy control bar a ToggleCommand again
    Commands defined in the main application context are not automatically
    configured. This means that we have to do it in a roundabout way,
    using the LifeCycleAdvisor. This is a prety bad kluge, but I can't
    see any better option

---
+++ src/org/owasp/webscarab/plugins/proxy/swing/ShowProxyControlBarToggleCommand.java
@@ -0,0 +1,49 @@
+/**
+ *
+ */
+package org.owasp.webscarab.plugins.proxy.swing;
+
+import javax.swing.JWindow;
+
+import org.springframework.richclient.application.config.ApplicationLifecycleAdvisor;
+import org.springframework.richclient.command.ToggleCommand;
+
+/**
+ * @author rdawes
+ *
+ */
+public class ShowProxyControlBarToggleCommand extends ToggleCommand {
+
+	private ProxyControlBar proxyControlBar;
+
+	public ShowProxyControlBarToggleCommand() {
+	}
+
+	public ProxyControlBar getProxyControlBar() {
+		return this.proxyControlBar;
+	}
+
+	public void setProxyControlBar(ProxyControlBar proxyControlBar) {
+		this.proxyControlBar = proxyControlBar;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.richclient.command.ToggleCommand#onSelection(boolean)
+	 */
+	@Override
+	protected boolean onSelection(boolean selected) {
+		JWindow window = proxyControlBar.getControl();
+		window.setVisible(selected);
+		return selected;
+	}
+
+	/**
+	 * @param applicationLifecycleAdvisor the applicationLifecycleAdvisor to set
+	 */
+	public void setApplicationLifecycleAdvisor(
+			ApplicationLifecycleAdvisor applicationLifecycleAdvisor) {
+		applicationLifecycleAdvisor.createWindowCommandManager().configure(this);
+	}
+
+
+}
