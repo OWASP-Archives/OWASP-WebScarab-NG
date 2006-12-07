6fde4d24afffca7634ed186c88db376738cb71f2
Rogan Dawes <rogan@dawes.za.net>
2006-12-07 11:09:07 +0200

    Throw an exception if we are passed an unexpected value, rather than just returning null, and hoping for a NullPointerException at some later stage

--- src/main/java/org/owasp/webscarab/jdbc/VersionDao.java
+++ src/main/java/org/owasp/webscarab/jdbc/VersionDao.java
@@ -1,7 +1,9 @@
 /**
- * 
+ *
  */
 package org.owasp.webscarab.jdbc;
+
+import org.springframework.dao.DataRetrievalFailureException;
 
 /**
  * @author rdawes
@@ -11,17 +13,17 @@ public class VersionDao {
 
     public static final Integer VERSION_0 = new Integer(0);
     public static final Integer VERSION_1 = new Integer(1);
-    
+
     public Integer getId(String version) {
         if ("HTTP/1.0".equals(version)) return VERSION_0;
         if ("HTTP/1.1".equals(version)) return VERSION_1;
-        return null;
+        throw new DataRetrievalFailureException("Unknown HTTP version: " + version);
     }
-    
+
     public String getVersion(Integer id) {
         if (VERSION_0.equals(id)) return "HTTP/1.0";
         if (VERSION_1.equals(id)) return "HTTP/1.1";
-        return null;
+        throw new DataRetrievalFailureException("Unknown HTTP version id: " + id);
     }
-    
+
 }
