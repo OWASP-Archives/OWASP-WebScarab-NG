4865fbd02c1d70a96bdd42bd6966717bc2383cef
Rogan Dawes <rogan@dawes.za.net>
2007-01-15 23:50:03 +0200

    Split more functionality out of ConversationView
    We now have a ConversationList View that follows SiteMap
    selection events, and filters the conversation list
    accordingly, as well as publishing the selection events.
    
    Now ConversationView is only responsible for listening for
    ConversationSelectionEvent's, and acting on them appropriately.

--- src/main/java/org/owasp/webscarab/ui/ConversationView.java
+++ src/main/java/org/owasp/webscarab/ui/ConversationView.java
@@ -4,21 +4,10 @@
 package org.owasp.webscarab.ui;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
-import java.net.URI;
-import java.util.List;
 
 import javax.swing.JComponent;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 
 import org.bushe.swing.event.EventService;
 import org.bushe.swing.event.EventServiceEvent;
@@ -32,21 +21,9 @@ import org.owasp.webscarab.ui.forms.support.ConversationFormSupport;
 import org.springframework.binding.form.CommitListener;
 import org.springframework.binding.form.FormModel;
 import org.springframework.richclient.application.PageComponent;
-import org.springframework.richclient.application.PageComponentContext;
 import org.springframework.richclient.application.support.AbstractView;
-import org.springframework.richclient.command.support.AbstractActionCommandExecutor;
 import org.springframework.richclient.form.Form;
 import org.springframework.richclient.form.FormModelHelper;
-
-import ca.odell.glazedlists.EventList;
-import ca.odell.glazedlists.FilterList;
-import ca.odell.glazedlists.SortedList;
-import ca.odell.glazedlists.TextFilterator;
-import ca.odell.glazedlists.matchers.AbstractMatcherEditor;
-import ca.odell.glazedlists.matchers.Matcher;
-import ca.odell.glazedlists.matchers.MatcherEditor;
-import ca.odell.glazedlists.swing.EventSelectionModel;
-import ca.odell.glazedlists.swing.TextComponentMatcherEditor;
 
 /**
  * @author rdawes
@@ -60,23 +37,15 @@ public class ConversationView extends AbstractView {
 
 	private Form annotationForm;
 
-	private EventList<Conversation> conversationList;
-
     private EventService eventService;
 
 	private ConversationService conversationService;
-
-	private ConversationTableFactory conversationTableFactory;
-
-	private Conversation selectedConversation;
 
 	private FormModel conversationModel;
 
 	private FormModel annotationModel;
 
-	private JPanel filterPanel;
-
-	private FindExecutor findExecutor = new FindExecutor();
+    private Listener listener = new Listener();
 
 	/*
 	 * (non-Javadoc)
@@ -98,83 +67,16 @@ public class ConversationView extends AbstractView {
 
 		annotationModel.addCommitListener(new AnnotationListener());
 
-		JTextField filterField = getComponentFactory().createTextField();
-		UriMatcher uriMatcher = new UriMatcher();
-
-		EventList<Conversation> conversationList = getConversationList();
-		FilterList<Conversation> uriFilterList = new FilterList(conversationList, uriMatcher);
-		TextFilterator<Conversation> filterator = new ConversationFilter();
-		MatcherEditor<Conversation> matcher = new TextComponentMatcherEditor<Conversation>(filterField, filterator);
-		FilterList<Conversation> filterList = new FilterList(uriFilterList, matcher);
-		SortedList<Conversation> sortedList = new SortedList<Conversation>(filterList);
-
-		JPanel panel = getComponentFactory().createPanel(new BorderLayout());
-		JSplitPane mainSplitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
-		mainSplitPane.setResizeWeight(0.5);
-
-		filterPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		filterPanel.add(getComponentFactory().createLabelFor("filter", filterField));
-		filterPanel.add(filterField);
-
-		JTable table = getConversationTableFactory().getConversationTable(sortedList);
-		final EventSelectionModel<Conversation> conversationSelectionModel = new EventSelectionModel<Conversation>(
-				sortedList);
-		table.setSelectionModel(conversationSelectionModel);
-		JScrollPane tableScrollPane = getComponentFactory().createScrollPane(
-				table);
-		tableScrollPane.setMinimumSize(new Dimension(100, 60));
-		JPanel topPanel = new JPanel(new BorderLayout());
-		topPanel.add(tableScrollPane, BorderLayout.CENTER);
-		topPanel.add(filterPanel, BorderLayout.SOUTH);
-		mainSplitPane.setTopComponent(topPanel);
-
-		table.getSelectionModel().addListSelectionListener(
-			new ListSelectionListener() {
-				public void valueChanged(ListSelectionEvent e) {
-					if (e.getValueIsAdjusting())
-						return;
-					EventList<Conversation> selected = conversationSelectionModel
-							.getSelected();
-					if (selected.isEmpty() || selected.size() > 1) {
-						updateSelection(null);
-					} else {
-						updateSelection(selected.get(0));
-					}
-				}
-			}
-		);
-		table.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				findExecutor.setEnabled(true);
-			}
-			@Override
-			public void focusLost(FocusEvent e) {
-//				findExecutor.setEnabled(false);
-			}
-		});
 		JSplitPane conversationSplitPane = new JSplitPane();
 		conversationSplitPane.setOneTouchExpandable(true);
 		conversationSplitPane.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
 		conversationSplitPane.setResizeWeight(0.5);
 		conversationSplitPane.setTopComponent(requestForm.getControl());
 		conversationSplitPane.setBottomComponent(responseForm.getControl());
-		mainSplitPane.setBottomComponent(conversationSplitPane);
-		panel.add(mainSplitPane, BorderLayout.CENTER);
+        JPanel panel = getComponentFactory().createPanel(new BorderLayout());
+		panel.add(conversationSplitPane, BorderLayout.CENTER);
 		panel.add(annotationForm.getControl(), BorderLayout.SOUTH);
-		if (getSelectedConversation() != null) {
-			conversationSelectionModel.getSelected().add(getSelectedConversation());
-		}
 		return panel;
-	}
-
-
-	/* (non-Javadoc)
-	 * @see org.springframework.richclient.application.support.AbstractView#registerLocalCommandExecutors(org.springframework.richclient.application.PageComponentContext)
-	 */
-	@Override
-	protected void registerLocalCommandExecutors(PageComponentContext context) {
-        context.register("findCommand", findExecutor);
 	}
 
 
@@ -197,23 +99,6 @@ public class ConversationView extends AbstractView {
 		}
 	}
 
-	public EventList<Conversation> getConversationList() {
-		return this.conversationList;
-	}
-
-	public void setConversationList(
-			EventList<Conversation> conversationList) {
-		this.conversationList = conversationList;
-	}
-
-	public Conversation getSelectedConversation() {
-		return this.selectedConversation;
-	}
-
-	public void setSelectedConversation(Conversation selectedConversation) {
-		this.selectedConversation = selectedConversation;
-	}
-
 	/**
 	 * @return Returns the conversationService.
 	 */
@@ -222,16 +107,6 @@ public class ConversationView extends AbstractView {
 			conversationService = (ConversationService) getApplicationContext()
 					.getBean("conversationService");
 		return conversationService;
-	}
-
-	/**
-	 * @return Returns the conversationTableFactory.
-	 */
-	public ConversationTableFactory getConversationTableFactory() {
-		if (conversationTableFactory == null)
-			conversationTableFactory = (ConversationTableFactory) getApplicationContext()
-					.getBean("conversationTableFactory");
-		return conversationTableFactory;
 	}
 
 	private class AnnotationListener implements CommitListener {
@@ -243,82 +118,47 @@ public class ConversationView extends AbstractView {
 		}
 	}
 
-	private class ConversationFilter implements TextFilterator<Conversation> {
-
-		public void getFilterStrings(List<String> list, Conversation conversation) {
-			list.add(conversation.getRequestMethod());
-			list.add(conversation.getRequestUri().toString());
-			list.add(conversation.getResponseStatus());
-			list.add(conversation.getResponseMessage());
-			list.add(conversation.getSource());
-			Annotation annotation = getConversationService().getAnnotation(conversation.getId());
-			if (annotation != null && !"".equals(annotation.getAnnotation()))
-				list.add(annotation.getAnnotation());
-		}
-
-	}
-
-	private class FindExecutor extends AbstractActionCommandExecutor {
-		public void execute() {
-			filterPanel.setVisible(true);
-		}
-	}
-
-	private class UriMatcher extends AbstractMatcherEditor<Conversation> {
-
-		private Matcher<Conversation> matcher;
-
-        private URI[] selection = new URI[0];
-
-		public UriMatcher() {
-			matcher = new Matcher<Conversation>() {
-				public boolean matches(Conversation conversation) {
-					if (selection.length == 0) return true;
-					for (int i=0; i<selection.length; i++) {
-						if (conversation.getRequestUri().toString().startsWith(selection[i].toString()))
-							return true;
-					}
-					return false;
-				}
-			};
-            eventService.subscribeStrongly(URISelectionEvent.class, new SwingEventSubscriber() {
-                protected void handleEventOnEDT(EventServiceEvent evt) {
-                    if (evt instanceof URISelectionEvent) {
-                        URISelectionEvent use = (URISelectionEvent) evt;
-                        Object source = use.getSource();
-                        if (source instanceof PageComponent) {
-                            PageComponent pc = (PageComponent) source;
-                            if (pc.getContext().getPage().equals(getContext().getPage())) {
-                                selection = use.getSelection();
-                                for (int i=0; i<selection.length; i++)
-                                    System.out.println("Sel["+i+"] : " + selection[i]);
-                                if (selection.length == 0) {
-                                    fireMatchNone();
-                                } else {
-                                    fireChanged(matcher);
-                                }
-                            }
-                        }
-                    }
-                }
-
-            });
-		}
-
-		/* (non-Javadoc)
-		 * @see ca.odell.glazedlists.matchers.AbstractMatcherEditor#getMatcher()
-		 */
-		@Override
-		public Matcher<Conversation> getMatcher() {
-			return matcher;
-		}
-
-	}
-
     /**
      * @param eventService the eventService to set
      */
     public void setEventService(EventService eventService) {
         this.eventService = eventService;
+        if (eventService != null) {
+            eventService.subscribe(ConversationSelectionEvent.class, listener);
+        }
     }
+
+    /**
+     * @return the eventService
+     */
+    public EventService getEventService() {
+        return this.eventService;
+    }
+
+    private class Listener extends SwingEventSubscriber {
+
+        /* (non-Javadoc)
+         * @see org.owasp.webscarab.ui.SwingEventSubscriber#handleEventOnEDT(org.bushe.swing.event.EventServiceEvent)
+         */
+        @Override
+        protected void handleEventOnEDT(EventServiceEvent evt) {
+            if (!(evt instanceof ConversationSelectionEvent))
+                return;
+            ConversationSelectionEvent cse = (ConversationSelectionEvent) evt;
+            Object source = cse.getSource();
+            if (!(source instanceof PageComponent))
+                return;
+            PageComponent pc = (PageComponent) source;
+            if (!pc.getContext().getPage().equals(getContext().getPage()))
+                return;
+            Conversation[] selection = cse.getSelection();
+            if (selection == null || selection.length != 1) {
+                updateSelection(null);
+            } else {
+                updateSelection(selection[0]);
+            }
+        }
+
+    }
+
 }
