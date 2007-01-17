ea18767357990890113da039ebd4317c3624862f
Rogan Dawes <rogan@dawes.za.net>
2007-01-17 18:01:11 +0200

    Make SiteMapView only show "successful" conversations
    I.e. only show conversations with a response status
    starting with 2xx or 3xx.
    
    Also make sure that we only remove items from the tree when
    the last instance has been removed from the list. This will
    be necessary when we filter conversations based on whether they
    are images or not.

--- src/main/java/org/owasp/webscarab/ui/SiteMapView.java
+++ src/main/java/org/owasp/webscarab/ui/SiteMapView.java
@@ -24,8 +24,11 @@ import org.owasp.webscarab.util.swing.renderers.UriRenderer;
 import org.springframework.richclient.application.support.AbstractView;
 
 import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.FilterList;
 import ca.odell.glazedlists.event.ListEvent;
 import ca.odell.glazedlists.event.ListEventListener;
+import ca.odell.glazedlists.matchers.AbstractMatcherEditor;
+import ca.odell.glazedlists.matchers.Matcher;
 
 /**
  * @author rdawes
@@ -35,7 +38,7 @@ public class SiteMapView extends AbstractView {
 
     private ArrayList<URI> uris;
 
-    private EventList<Conversation> conversationList;
+    private FilterList<Conversation> conversationList;
 
     private EventService eventService;
 
@@ -76,12 +79,17 @@ public class SiteMapView extends AbstractView {
      *            the conversationList to set
      */
     public void setConversationList(EventList<Conversation> conversationList) {
-        if (this.conversationList != null)
+        if (this.conversationList != null) {
             this.conversationList.removeListEventListener(listener);
-        this.conversationList = conversationList;
+            this.conversationList.dispose();
+        }
         if (conversationList != null) {
+            this.conversationList = new FilterList<Conversation>(
+                    conversationList, new SuccessfulConversationMatcherEditor());
             populateExisting();
-            conversationList.addListEventListener(listener);
+            this.conversationList.addListEventListener(listener);
+        } else {
+            this.conversationList = null;
         }
     }
 
@@ -102,7 +110,8 @@ public class SiteMapView extends AbstractView {
         conversationList.getReadWriteLock().readLock().unlock();
     }
 
-    private class Listener implements ListEventListener<Conversation>, TreeSelectionListener {
+    private class Listener implements ListEventListener<Conversation>,
+            TreeSelectionListener {
 
         public void valueChanged(TreeSelectionEvent e) {
             TreePath[] paths = uriTree.getSelectionPaths();
@@ -111,7 +120,7 @@ public class SiteMapView extends AbstractView {
                 use = new URISelectionEvent(SiteMapView.this, new URI[0]);
             } else {
                 URI[] sel = new URI[paths.length];
-                for (int i=0; i<paths.length; i++) {
+                for (int i = 0; i < paths.length; i++) {
                     sel[i] = (URI) paths[i].getLastPathComponent();
                 }
                 use = new URISelectionEvent(SiteMapView.this, sel);
@@ -128,15 +137,43 @@ public class SiteMapView extends AbstractView {
             while (evt.next()) {
                 int index = evt.getIndex();
                 if (evt.getType() == ListEvent.DELETE) {
-                    uriTreeModel.remove(uris.remove(index));
+                    URI uri = uris.remove(index);
+                    if (!uris.contains(uri))
+                        uriTreeModel.remove(uri);
                 } else if (evt.getType() == ListEvent.INSERT) {
                     URI uri = conversationList.get(index).getRequestUri();
+                    if (!uris.contains(uri))
+                        uriTreeModel.add(uri);
                     uris.add(index, uri);
-                    uriTreeModel.add(uri);
                 }
             }
         }
 
     }
 
+    private class SuccessfulConversationMatcherEditor extends
+            AbstractMatcherEditor<Conversation> {
+
+        private Matcher<Conversation> matcher;
+
+        public SuccessfulConversationMatcherEditor() {
+            matcher = new Matcher<Conversation>() {
+                public boolean matches(Conversation conversation) {
+                    String status = conversation.getResponseStatus();
+                    switch (status.charAt(0)) {
+                    case '2':
+                        return true;
+                    case '3':
+                        return true;
+                    default:
+                        return false;
+                    }
+                }
+            };
+        }
+
+        public Matcher<Conversation> getMatcher() {
+            return matcher;
+        }
+    }
 }
